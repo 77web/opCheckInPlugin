a8225c64d1ca01647a5d6d9f35d836f146299f62
77web <info@77-web.com>
2011-06-13 08:25:42 +0900

    fixed show template

--- apps/pc_frontend/modules/checkIn/templates/showSuccess.php
+++ apps/pc_frontend/modules/checkIn/templates/showSuccess.php
@@ -1,8 +1,20 @@
 <?php slot('checkInBox'); ?>
-<?php echo link_to($checkIn->getCheckInSpot()->getName(), '@checkin_spot?id='.$checkIn->getCheckInSpotId()); ?>
+<table>
+  <tr>
+    <th><?php echo __('%checkin% date'); ?></th>
+    <td><?php echo op_format_date($checkIn->getCreatedAt(), 'XDateTimeJa'); ?></td>
+  </tr>
+  <tr>
+    <th><?php echo __('%checkin% spot'); ?></th>
+    <td>
+      <?php echo link_to($checkIn->getCheckInSpot()->getName(), '@checkin_spot?id='.$checkIn->getCheckInSpotId()); ?>
+    </td>
+  </tr>
+</table>
 
 <?php echo op_decoration(nl2br($checkIn->getBody())); ?>
+
 <?php end_slot(); ?>
 
 
-<?php op_include_box('checkInDetailBox', get_slot('checkInBox'), array('title' => __('Detail of %%'))); ?>
+<?php op_include_box('checkInDetailBox', get_slot('checkInBox'), array('title' => __('%member%\'s %checkin%', array('%member%'=>$checkIn->getMember()->getName())))); ?>
